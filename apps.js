9eb591a0f2a318980c303fc9d28ec25973932450
tonmoy <tonmoy3673@gmail.com>
2025-02-25 00:31:49 +0600

    :tada: create digital clock ui

--- apps.js
+++ apps.js
@@ -22,3 +22,34 @@ buttons.forEach(button=>{
 
     })
 })
+
+// ================== Change Button Color ==============//
+
+
+const ChangeBoxColor =()=>{
+  const boxContainer = document.getElementById('box');
+  boxContainer.style.cssText = 'height: 200px; width: 200px; background-color: red; margin: 0 auto; border-radius: 6px;';
+  const button = document.getElementById('btn');
+  button.style.cursor='pointer';
+ button.addEventListener('click',function(){
+  const inputValue = document.getElementById('input').value;
+if(isValidColor(inputValue)){
+  boxContainer.style.backgroundColor= inputValue;
+
+}
+   else{
+    alert('Invalid Color!!')
+   }
+ })
+ 
+};
+
+const isValidColor =(color)=>{
+ const testDiv = document.createElement('div');
+  testDiv.style.color=color;
+  return testDiv.style.color !== '';
+
+}
+
+// =========== Call the Function =======//
+ChangeBoxColor();
